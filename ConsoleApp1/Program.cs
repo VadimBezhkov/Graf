8dab827e6b71aa67757073e0e4629b3bab5552c7
Вадим Бежков <vadimbezhkov3112@gmail.com>
2020-12-06 01:05:23 +0300

    One commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+   
+       public class Timer
+        {
+            public int Hours { get; set; }
+            public int Minuts { get; set; }
+            public int Seconds { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Time mytime = new Time { Seconds = 103 };
+            //int x = mytime;
+            //Time mytime1 = (Time)x;
+            Timer timer1 = mytime;
+            Time mytime2 = timer1;
+            Console.WriteLine($"{timer1.Hours}:{timer1.Minuts}:{timer1.Seconds}");
+            Console.WriteLine(mytime2.Seconds);
+
+            Console.ReadKey();
+        }
+        class Time
+        {
+            public int Seconds { get; set; }
+            public static explicit operator Time(int x)
+            {
+                return new Time { Seconds = x };
+            }
+            public static implicit operator int(Time time)
+            {
+                return time.Seconds;
+            }
+            public static implicit operator Time(Timer timer)
+            {
+                int h = timer.Hours*3600;
+                int m = timer.Minuts * 60;
+                return new Time { Seconds = h + m + timer.Seconds };
+            }
+            public static implicit operator Timer(Time time)
+            {
+                int h = time.Seconds / 3600;
+                int m = (time.Seconds-h*3600) / 60;
+                int s = time.Seconds - h * 3600 - m * 60;
+                return new Timer {Hours=h , Minuts = m, Seconds= s};
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class User
+    {
+        public int value { get; set; }
+        public static User operator ++(User user)
+        {
+            return new User { value = user.value + 10 };
+        }
+        public static User operator + (User user,User user1)
+        {
+            return new User {value = user.value+user1.value };
+        }
+        public static int operator +(User user, int x)
+        {
+            return user.value + x ;
+        }
+        public static bool operator > (User user, User user1)
+        {
+            if (user.value > user1.value)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(User user, User user1)
+        {
+            if (user.value < user1.value)
+                return true;
+            else
+                return false;
+        }
+        public static int operator+(int y,User user1)
+        {
+            return y + user1.value;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User {value = 12};
+            User user1 = new User {value = 15};
+            User user2 = user + user1;
+            User user3 = new User { value = 1 };
+            user++;
+            Console.WriteLine("sdads"+user.value);
+            Console.WriteLine(user.value);
+            Console.WriteLine(user2.value);
+            bool result = user > user1;
+            Console.WriteLine(result) ;
+            user1 = user1++;
+        
+            int x = user + 40;
+            int y = 99 + user1;
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
